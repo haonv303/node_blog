a2401491d3197d3b801a758160e1f57b5a15c4c6
haong <haonvps24050@fpt.edu.vn>
2023-08-26 00:10:03 +0700

    add controller & route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const app = express();
 const morgan = require("morgan");
 const port = 8080;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
@@ -24,18 +26,7 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 
 app.set("views", path.join(__dirname, "resources/views"));
-app.get("/", (req, res) => {
-  return res.render("home");
-});
 
-app.get("/search", (req, res) => {
-  return res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  // return res.render("search");
-  console.log(req.body);
-  res.send("");
-});
+route(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
